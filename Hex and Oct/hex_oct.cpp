388ca4ef7c760a9e8b0568b0deb37e0e2a21b0e0
Yann Kamche <yannkamche@gmail.com>
2023-04-15 15:00:21 +0100

    Hex and Oct representations

---
+++ Hex and Oct/hex_oct.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::hex;
+using std::oct;
+using std::endl;
+
+int main() {
+    int myHex = 0x15;
+    cout << myHex << endl;
+
+    int myOct = 020;
+    cout << myOct << endl;
+
+
+}
